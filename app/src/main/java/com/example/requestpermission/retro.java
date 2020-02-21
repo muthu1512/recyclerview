9a4af79e1cfe6c11a9138552b223a762cc34a280
muthu1512 <muthurko15@gmail.com>
2020-02-21 18:07:15 +0530

    retrofit final step

--- app/src/main/java/com/example/requestpermission/retro.java
+++ app/src/main/java/com/example/requestpermission/retro.java
@@ -23,7 +23,7 @@ public class retro extends Fragment {
     private RecyclerView rv;
     private retro_adapter retroAdapter;
     private ProgressBar progressBar;
-    int i = 1, p = 0;
+    int i = 1, p = 1;
     int count = 10;
     public retro() {
     }
